948773648e2a5c2727b299419f12221a9c366aa8
MinhHieu012 <0967710509hieu@gmail.com>
2024-04-09 11:31:26 +0700

    Done bug: Hien thi modal chon/them khach hang va tim kiem

--- src/cart-view/search-customer-modal/style.js
+++ src/cart-view/search-customer-modal/style.js
@@ -1,11 +1,12 @@
-import StyleSheet from "react-native"
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    modalUser: {
-        position: 'absolute',
-        top: "9%",
-        width: "90%",
-        height: "45%"
-    }
-})
-export default styles
+  modalUser: {
+    position: "absolute",
+    top: "9%",
+    width: "90%",
+    height: "45%",
+  },
+});
+
+export default styles;
